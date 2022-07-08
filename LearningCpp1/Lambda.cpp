fb663b8ca81f2fc365dbe53bcb607af01f704ac0
Mustafa Selçuk Çağlar <11412546+develooper1994@users.noreply.github.com>
2022-07-08 16:27:02 +0300

    Amended

--- LearningCpp1/Lambda.cpp
+++ LearningCpp1/Lambda.cpp
@@ -1,6 +1,13 @@
 #include "Lambda.h"
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <chrono>
+#include <typeinfo>
+#include <type_traits>
 
 /*
 * NOTE: It is like a function pointers but i can threat them as a objects. Lambda functions and function pointers are useful as callback functions.
