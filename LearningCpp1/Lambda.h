8335bff9da914abcc9988c1bbe546d2408fb5b99
Mustafa Selçuk Çağlar <11412546+develooper1994@users.noreply.github.com>
2022-08-04 15:10:40 +0300

    Lambda Has Finished

--- LearningCpp1/Lambda.h
+++ LearningCpp1/Lambda.h
@@ -1,3 +1,66 @@
 #pragma once
 
+#include <iostream>
+#include <iosfwd>
+#include <string>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <random>
+#include <chrono>
+#include <typeinfo>
+#include <type_traits>
+
+using std::vector;
+using std::array;
+using std::cout;
+using std::ostream;
+
+//template<typename T, char deliminator = ',', int maxLineWidth = 16>
+//std::ostream operator<<(std::ostream& out, const std::vector<T>& vec) {
+//	out << "{";
+//
+//	size_t last = vec.size() - 1;
+//	for (size_t i = 0; i < vec.size(); i++) {
+//		out << vec[i];
+//		if (i != last)
+//			out << ", ";
+//		if (i % maxLineWidth == maxLineWidth - 1)
+//			out << '\n';
+//	}
+//
+//	out << "}";
+//}
+//
+//template<typename T, int size, char deliminator = ',', int maxLineWidth = 16>
+//std::ostream operator<<(std::ostream& out, const std::array<T, size>& vec) {
+//	out << "{";
+//
+//	size_t last = size - 1;
+//	for (size_t i = 0; i < vec.size(); i++) {
+//		out << vec[i];
+//		if (i != last)
+//			out << ", ";
+//		if (i % maxLineWidth == maxLineWidth - 1)
+//			out << '\n';
+//	}
+//
+//	out << "}";
+//}
+//
+//template<typename T, int size, char deliminator = ',', int maxLineWidth = 16>
+//std::ostream& operator<< (std::ostream& out, const T(&v)[size]) {
+//	out << '{';
+//	size_t last = size - 1;
+//	for (size_t i = 0; i < size; ++i) {
+//		out << v[i];
+//		if (i != last)
+//			out << deliminator << ' ';
+//		if (i % maxLineWidth == maxLineWidth - 1)
+//			out << '\n';
+//	}
+//	out << '}';
+//	return out;
+//}
+
 void Lambda_Main();
